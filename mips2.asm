29bfb9436a4ae2e471d7d889fbf44eb901a797cc
Daniel Nunez <daniel.nunez@nave.rs>
2022-11-06 03:02:59 -0300

    feat: add print board func

--- mips2.asm
+++ mips2.asm
@@ -2,16 +2,23 @@
 	player: .asciiz "Jogador "
 	doMove: .asciiz ", faça sua jogada: "
 	newLine: .asciiz "\n"
-	currentMove: .space 3		# xy\n
+	currentMove: .space 3		# xy\n (BUFFER DE STRING)
+	
+	board: .word line1, line2, line3
+	line1: .word 0, 1, 2
+	line2: .word 3, 4, 5
+	line3: .word 6, 7, 8
 .text
 main:
 	addi $t0, $zero, 1		# t0 = jogador atual
+	la $t1, board			# t1 = endereço de board
 	addi $t9, $zero, 0		# t9 = numero de jogadas realizadas
 gameLoop:
 	jal printPlayerMessage
 	jal readPlayerMove
 	# move's validation
 	# print game
+	jal printBoard
 	# verifyWin
 	jal verifyEnd
 	jal togglePlayer
@@ -58,8 +65,37 @@ togglePlayer:
 	jr $ra
 
 verifyEnd:
-	beq $t9, 2, end
+	beq $t9, 9, end
 	jr $ra
+
+printBoard:
+	addi $t4, $zero, 0		# index row
+	addi $t5, $zero, 0		# index column
+	loopRow:	beq $t4, 3, endLoopRow
+			sll $t8, $t4, 2		# multiplicador para acessar posicao na memoria (4 em 4) [linha]
+			la $t2, board($t8)	# endereço da linha atual
+
+	loopColumn: 	beq $t5, 3, endLoopColumn
+			# acessar row e coluna no array (pseudo-matriz)
+			sll $t8, $t5, 2		# multiplicador para acessar posicao na memoria (4 em 4) [coluna]
+			
+			lw $t6, 0($t2)
+			add $t6, $t6, $t8
+			lw $t7, 0($t6)
+				
+			li $v0, 1
+			move $a0, $t7
+			syscall
+			addi $t5, $t5, 1
+			j loopColumn
+	endLoopColumn:	li $a0, newLine
+			li $v0, 11
+			syscall
+			addi $t5, $zero, 0
+			addi $t4, $t4, 1
+			j loopRow
+	endLoopRow:
+		jr $ra
 	
 end: sll $0, $0, 0
 	
